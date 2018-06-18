6f3a08cbe4ef2ac4298aea4294b7eeda2b2c02a1
Mack <ryanmcilvenna@Ryans-MacBook-Pro.local>
2018-06-18 16:37:04 -0700

    initial submit

---
+++ assets/app.js
@@ -0,0 +1,41 @@
+var questionArr = [
+    {   question:"How many colours are there in a rainbow?",
+        choices:['One','Five','Seven','Nine'],
+        ans:'Seven'
+    },
+    {   question:"What do you call a time span of one thousand years?",
+        choices:['Years','Continuum','Decade','Millenium'],
+        ans:'Millenium'
+    },
+    {   question:"How many degrees are found in a circle?",
+        choices:['360','90','540','180'],
+        ans:'360'
+    },
+    {   question:"How many points does a compass have?",
+        choices:['51','32','12','40'],
+        ans:'32'
+    },
+    {   question:"What is the bluebird of symbol of?",
+        choices:['Happiness','Joy','Anger','Love'],
+        ans:'Happiness'
+    },
+    {   question:"When did the world celebrate its most recent millennium?",
+        choices:['2010','1990','1950','2000'],
+        ans:'2000'
+    },
+    {   question:"Name the world's largest ocean?",
+        choices:['Red Sea','Atlantic Ocean','Pacific Ocean','Atlantic Ocean'],
+        ans:'Pacific Ocean'
+    },
+    {   question:"What is the world's longest river?",
+        choices:['Nicaraquan River','Sacramento River','Amazon River','Whitewater River'],
+        ans:'Amazon River'
+    },
+    {   question:"What is the capital city of Spain?",
+        choices:['Madrid','Montilla','San Fernando','Algeciras'],
+        ans:'Madrid'
+    },
+    {   question:"What colour jersey is worn by the winners of each stage of the Tour De France?",
+        choices:['Orange','Yellow','Blue','Green'],
+        ans:'Yellow'
+    }],
